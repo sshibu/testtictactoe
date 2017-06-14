11390c513de45a9dfc601825d722f66e691de72a
Sherin Shibu <sherin@Sherins-MacBook-Pro.local>
2017-06-14 16:44:12 -0700

    shared scheme commit

--- TicTacToe/AppDelegate.swift
+++ TicTacToe/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import MobileCenter
+import MobileCenterAnalytics
+import MobileCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSMobileCenter.start("1962bb7c-47e3-4f75-a9df-aba4782e91c4", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
         return true
     }
 
